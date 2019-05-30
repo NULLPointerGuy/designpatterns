d336d7ea36a932354865621d2e0e02ac59db0e24
Karthik R <r.karthik@redbus.com>
2019-05-30 18:06:11 +0530

    Facade: rewritten

--- design/src/Structural/Facade.kt
+++ design/src/Structural/Facade.kt
@@ -7,97 +7,43 @@ package Structural
  * implementation is hidden behind `ShapeArea` interface.
  */
 fun main(){
-    val facade = FacadeNavigator(circle(11f), square(12f),rectangle(13f,14f))
-
-    /**
-     * facade Navigator decides where to redirect the getShapeArea() method call
-     */
-    println("${facade.getShapeArea("Circle")}")
-    println("${facade.getShapeArea("Square")}")
-    println("${facade.getShapeArea("Rectangle")}")
+    CarFacade().startCar()
 }
 
 
-interface ShapeArea{
-    fun getShapeArea():Float
-}
+class CarFacade{
+    private val engine:Engine
+    private val battery:Battery
+    private val carDashBoard:CarDashBoard
 
-
-class FacadeNavigator {
-
-    private val circle: circle
-    private val square: square
-    private val rectangle: rectangle
-
-    /**
-     * Usually these instance are created using factory pattern, but for the
-     * example purpose we will just inject in constructor.
-     */
-    constructor(circle: circle,square: square,rectangle: rectangle){
-        this.circle = circle
-        this.rectangle = rectangle
-        this.square = square
+    constructor(){
+        engine = Engine()
+        battery = Battery()
+        carDashBoard = CarDashBoard()
     }
 
-    fun getShapeArea(shape:String):Float{
-        return when(shape){
-            "Circle"->{
-                this.circle.getShapeArea()
-            }
-            "Square"->{
-                this.square.getShapeArea()
-            }
-            "Rectangle"->{
-                this.rectangle.getShapeArea()
-            }
-            else->{
-                println("Shape not recognized")
-                0f
-            }
-        }
+    fun startCar(){
+        engine.startEngine()
+        battery.turnOnBattery()
+        carDashBoard.turnOnDashBoard()
     }
 }
 
-class circle:ShapeArea{
-
-    private val radius:Float
-
-    constructor(radius:Float){
-        this.radius = radius
-    }
-
-
-    override fun getShapeArea(): Float {
-        return (22/7)*radius*radius
+class Engine(){
+    fun startEngine(){
+        println("Start Engine...")
     }
 }
 
-class rectangle:ShapeArea{
-
-    private val length:Float
-    private val width:Float
-
-    constructor(length:Float,width:Float){
-        this.length = length
-        this.width = width
+class Battery(){
+    fun turnOnBattery(){
+        println("Turn on battery...")
     }
-
-    override fun getShapeArea(): Float {
-        return length*width
-    }
-
 }
 
-class square:ShapeArea{
-
-    private val length:Float
-
-    constructor(length: Float){
-        this.length = length
+class CarDashBoard(){
+    fun turnOnDashBoard(){
+        println("Turn on DashBoard")
     }
-
-    override fun getShapeArea(): Float {
-        return length*length
-    }
-
 }
+
